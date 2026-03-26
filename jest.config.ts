ea9a9b98b720cbd5200eb7ba5c48c5ffe2c4aa89
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-26 22:40:34 +0200

    first commit

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@config/(.*)': '<rootDir>/src/config/$1',
+    '@modules/(.*)': '<rootDir>/src/modules/$1',
+    '@shared/(.*)': '<rootDir>/src/shared/$1',
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  verbose: true,
+};
+
+export default config;
